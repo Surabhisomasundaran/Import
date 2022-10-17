f2df97ec51a9fddef0f7d2b6255cc7af5e98cbed
Surabhi <Surabhi@LAPTOP-GG5FM8MO>
2022-10-17 15:31:23 +0530

    commit

---
+++ Importdemoproject/src/Aggregationassignment/Marks.java
@@ -0,0 +1,15 @@
+package Aggregationassignment;
+
+public class Marks 
+{
+
+	public static void main(String[] args) 
+	{
+		Subjects obj=new Subjects ();
+		obj.marksEnglish();
+		obj.marksHindi();
+		obj.marksMaths();
+
+	}
+
+}
---
+++ Importdemoproject/src/Aggregationassignment/Subjects.java
@@ -0,0 +1,20 @@
+package Aggregationassignment;
+
+public class Subjects 
+{
+	public void marksEnglish()
+	{
+		System.out.println("marks scored for english is 100");
+	}
+	public void marksHindi()
+	{
+		System.out.println("marks scored for hindi is 85");
+	}
+	public void marksMaths()
+	{
+		System.out.println("marks scored for english is 98");
+	}
+	
+	
+
+}
